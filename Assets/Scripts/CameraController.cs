779156ee772cb9f3cb52ca379cf302b980b882a0
AristideExe <aristidepr@gmail.com>
2025-11-20 14:48:37 +0100

    feat: add aiming with right mouse button click

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -4,6 +4,8 @@ public class CameraController : MonoBehaviour
 {
     [SerializeField] private float minVerticalOrientation = 0f;
     [SerializeField] private float maxVerticalOrientation = 90f;
+    [SerializeField] private float zoomSpeed = 10f;
+    [SerializeField] private Vector2 zoomLimits = new Vector2(1, 2);
     
     [SerializeField] private PlayerController player;
     [SerializeField] private Transform cameraTransform;
@@ -15,6 +17,15 @@ public class CameraController : MonoBehaviour
         //var verticalRotation = Mathf.Clamp(calculatedRotation, minVerticalOrientation, maxVerticalOrientation);
         
         transform.eulerAngles = new Vector3(calculatedRotation, player.transform.eulerAngles.y, 0f); 
+        
+        if (Input.GetMouseButton(1))
+        {
+            cameraTransform.localPosition = new Vector3(cameraTransform.localPosition.x, cameraTransform.localPosition.y, Mathf.Clamp(cameraTransform.localPosition.z + zoomSpeed * Time.deltaTime, -zoomLimits.y, -zoomLimits.x));
+        }
+        else
+        {
+            cameraTransform.localPosition = new Vector3(cameraTransform.localPosition.x, cameraTransform.localPosition.y, Mathf.Clamp(cameraTransform.localPosition.z - zoomSpeed * Time.deltaTime, -zoomLimits.y, -zoomLimits.x));
+        }
     }
 
     public void VerticalRecoil(float verticalRecoil)
