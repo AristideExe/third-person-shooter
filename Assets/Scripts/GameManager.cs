77785ddd61cd0a5c2e9a977fa3a81671c920e090
AristideExe <aristidepr@gmail.com>
2026-01-06 12:39:26 +0100

    refactor: make numberOfEnemiesPerWave variable serialized

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -9,9 +9,9 @@ public class GameManager : MonoBehaviour
 {
     [SerializeField] private List<Window> windows;
     [SerializeField] private GameObject enemyPrefab;
+    [SerializeField] private int numberOfEnemiesPerWave = 6;
 
     private int _numberOfEnemiesAlive;
-    private int _numberOfEnemiesPerWave = 6;
     private int _waveNumber;
     
     public int WaveNumber => _waveNumber;
@@ -24,11 +24,11 @@ public class GameManager : MonoBehaviour
     private void StartWave()
     {
         _waveNumber++;
-        for (int i = 0; i < _numberOfEnemiesPerWave; i++)
+        for (int i = 0; i < numberOfEnemiesPerWave; i++)
         {
             windows[Random.Range(0, windows.Count)].SpawnEnemy(enemyPrefab);
         }
-        _numberOfEnemiesAlive = _numberOfEnemiesPerWave;
+        _numberOfEnemiesAlive = numberOfEnemiesPerWave;
     }
     
     public void EnemyKilled()
@@ -36,7 +36,7 @@ public class GameManager : MonoBehaviour
         _numberOfEnemiesAlive--;
         if (_numberOfEnemiesAlive <= 0)
         {
-            _numberOfEnemiesPerWave = (int) Math.Round(_numberOfEnemiesPerWave * 1.3f);
+            numberOfEnemiesPerWave = (int) Math.Round(numberOfEnemiesPerWave * 1.3f);
             StartWave();
         }
     }
